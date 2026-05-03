9e24f3b7ab456f1db156e4b187516ccf673d5acd
sabbagh <mohammad.sabbagh@gmail.com>
2026-05-03 20:11:39 +0300

    new brand logo and icons

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'icon-192v2.png', 'icon-512v2.png'],
+      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
       manifest: {
         name: 'ASTE',
         short_name: 'ASTE',
@@ -26,21 +26,17 @@ export default defineConfig({
         dir: 'rtl',
         icons: [
           {
-            src: 'icon-192v2.png',
+            src: 'icon-192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'any maskable',
           },
           {
-            src: 'icon-512v2.png',
+            src: 'icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
           },
-        ],
-        screenshots: [
-          { src: '/mobile.png',  sizes: '390x844',   type: 'image/png', label: 'ASTE Mobile' },
-          { src: '/desktop.png', sizes: '1280x800',  type: 'image/png', form_factor: 'wide', label: 'ASTE Desktop' },
         ],
       },
       workbox: {
